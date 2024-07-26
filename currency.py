86c4a4279be7f1f02a785e6aed5e008d9be5b866
charles chou <767140+chouex@users.noreply.github.com>
2024-07-26 15:27:29 +0800

    Update currency.py

--- currency.py
+++ currency.py
@@ -207,8 +207,21 @@ def get_hsbc():
 
 def get_soicheong():
     result = Result()
+    headers = {
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6',
+        'cache-control': 'no-cache',
+        'pragma': 'no-cache',
+        'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    }
     response = requests.get(
-        'https://www.soicheong.com/index.php?g=Api&m=Exchange&a=getRate',
+        'https://www.soicheong.com/index.php?g=Api&m=Exchange&a=getRate',headers=headers
     ).json()
     for i in response['value']:
         if i['codenum'] == 'RMB': i['codenum'] = 'CNY'
@@ -311,7 +324,7 @@ def get_text():
 
     rates['mastercard'] = (get_mastercard())
     rates['hsbc'] = (get_hsbc())
-    #rates['soicheong'] = (get_soicheong())
+    rates['soicheong'] = (get_soicheong())
     rates['jcb'] = (get_jcb())
     rates['yahoo'] = get_yahoo()
 
